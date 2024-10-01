3514afb9a7accd08775f33578a0265126c2a56b0
MARIO FERNANDO CERNA NAJERA <mcernan@miumg.edu.gt>
2024-10-01 15:06:28 -0600

    Base Logica

--- src/main/java/umg/dem1/BaseIntegrales/BaseLogica.java
+++ src/main/java/umg/dem1/BaseIntegrales/BaseLogica.java
@@ -1,2 +1,17 @@
-package umg.dem1.BaseIntegrales;public class BaseLogica {
+package umg.dem1.BaseIntegrales;
+
+import java.util.function.Function;
+
+public class BaseLogica {
+
+    public double integrar(Function<Double, Double> funcion, double a, double b){
+
+        double h = (b - a) / 1000;
+        double suma = 0.5 * (funcion.apply(a) + funcion.apply(b));
+        for (int i = 1; i < 1000; i++) {
+            double x = a + i * h;
+            suma += funcion.apply(x);
+        }
+        return suma * h;
+    }
 }
--- src/main/java/umg/dem1/Main.java
+++ src/main/java/umg/dem1/Main.java
@@ -1,17 +1,79 @@
 package umg.dem1;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import umg.dem1.BaseIntegrales.BaseLogica;
+import net.objecthunter.exp4j.Expression;
+import net.objecthunter.exp4j.ExpressionBuilder;
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.xy.XYSeries;
+import org.jfree.data.xy.XYSeriesCollection;
+
+import javax.swing.*;
+import java.util.Scanner;
+import java.util.function.Function;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner scanner = new Scanner(System.in);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        // Solicitar la función al usuario
+        System.out.println("Introduce la función a integrar (por ejemplo, 'sin(x)'):");
+        String funcionStr = scanner.nextLine();
+
+        // Solicitar los límites de integración
+        System.out.println("Introduce el límite inferior de integración:");
+        double a = scanner.nextDouble();
+
+        System.out.println("Introduce el límite superior de integración:");
+        double b = scanner.nextDouble();
+
+        // Convertir la cadena de la función a una Function<Double, Double>
+        Function<Double, Double> funcion = x -> {
+            Expression e = new ExpressionBuilder(funcionStr)
+                    .variable("x")
+                    .build()
+                    .setVariable("x", x);
+            return e.evaluate();
+        };
+
+        BaseLogica calculadora = new BaseLogica();
+        double resultado = calculadora.integrar(funcion, a, b);
+
+        // Redondear el resultado a 3 dígitos decimales
+        System.out.printf("Resultado de la integral: %.3f%n", resultado);
+
+        // Crear la serie de datos para la función
+        XYSeries series = new XYSeries("Función");
+        for (double x = a; x <= b; x += (b - a) / 1000) {
+            series.add(x, funcion.apply(x));
         }
+
+        // Crear el conjunto de datos y añadir la serie
+        XYSeriesCollection dataset = new XYSeriesCollection();
+        dataset.addSeries(series);
+
+        // Crear el gráfico
+        JFreeChart chart = ChartFactory.createXYLineChart(
+                "Gráfico de la Función",
+                "X",
+                "Y",
+                dataset,
+                PlotOrientation.VERTICAL,
+                true,
+                true,
+                false
+        );
+
+        // Mostrar el gráfico en una ventana
+        SwingUtilities.invokeLater(() -> {
+            JFrame frame = new JFrame("Gráfico");
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.add(new ChartPanel(chart));
+            frame.pack();
+            frame.setLocationRelativeTo(null);
+            frame.setVisible(true);
+        });
     }
 }
